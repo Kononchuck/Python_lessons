d850e2aa85b50e14e7a55f16c8ed2e2a70aec00f
den <7735440@gmail.com>
2022-06-25 14:48:08 +0300

    code added

--- Lesson_3.py
+++ Lesson_3.py
@@ -1,5 +1,5 @@
 import re
-import pymorhhy2
+import pymorphy2
 #методами строк очистить текст от знаков препинания;
 
 text = open('text.txt', 'r')
@@ -13,7 +13,7 @@ text = text.split(' ')
 #привести все слова к нижнему регистру(map);
 
 text = list(map(str.lower, text))
-lemm_text = text
+for_lemm_text = text
 
 #получить из list пункта 3 dict, ключами которого являются слова, а значениями их количество появлений в тексте;
 
@@ -34,7 +34,14 @@ print(sorted(counts.items(), reverse=True, key=lambda x: x[1])[0:5])
 
 #вывести количество разных слов в тексте(set)
 differ_words = len(set(counts))
-print(differ_words)
+print("Всего разных слов: ", differ_words)
 
 
 #в пункте 3 дополнительно к приведению к нижнему регистру выполнить лемматизацию
+morph = pymorphy2.MorphAnalyzer()
+show_lemm = list()
+for i in for_lemm_text:
+    p=morph.parse(i)[0]
+    show_lemm.append(p.normal_form)
+print(for_lemm_text)
+print(show_lemm)
