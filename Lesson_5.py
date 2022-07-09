126c2b4642f87523ac7327f2847b8318d16fcf85
den <7735440@gmail.com>
2022-07-09 11:04:03 +0300

    code added

--- Lesson_5.py
+++ Lesson_5.py
@@ -8,33 +8,28 @@ from math import sqrt
 3. выводит самый большой простой делитель числа.
 '''
 
-n = int(input('Введите число: '))
-def natural_numbers():
-    if n > 0 and n < 1001:
-        print('Число в диапазоне от 1 до 1000')
-        if n < 2:
-            print('Число не простое!')
-        for i in range(2, int(n ** 0.5 + 1)):
-            if n % i == 0:
-                print('Число не простое!')
-        else:
-            print('Число простое!')
 
+def divisor_master():
+    n = int(input('Введите число: '))
+    del_list = []
+    del_list_2 = []
+    if n > 0 and n < 1001:
+        print('Число лежит в диапазоне от 1 до 1000')
+        for i in range(1, 1001):
+            if n % i == 0:
+                del_list.append(i)
+            else:
+                pass
+        print('Список всех делителей: ', del_list)
+        for i in del_list:
+            for j in range(2, i):
+                if i % j == 0:
+                    break
+            else:
+                del_list_2.append(i)
+        print('Наибольший простой делитель числа: ', sorted(del_list_2, reverse=True)[0])
     else:
         print('Число меньше 1 или больше 1000.')
 
-print(natural_numbers())
+divisor_master()
 
-
-
-
-#правильно
-def is_prime(a):
-    if a < 2:
-        return False
-    for i in range(2, int(a ** 0.5 + 1)):
-        if a % i == 0:
-            return False
-    else:
-        return True
-print(is_prime(int(input())))
