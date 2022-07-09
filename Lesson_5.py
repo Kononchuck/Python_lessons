2f9da8ef91144f119a4976715ecd0237dd422764
den <7735440@gmail.com>
2022-07-09 13:55:19 +0300

    code added

--- Lesson_5.py
+++ Lesson_5.py
@@ -1,4 +1,5 @@
-'''Необходимо реализовать модуль divisor_master. Все функции модуля принимают на
+'''
+Необходимо реализовать модуль divisor_master. Все функции модуля принимают на
 вход натуральные числа от 1 до 1000. Модуль содержит функции:
 1. проверка числа на простоту (простые числа - это те числа у которых делители
 единица и они сами);
@@ -7,27 +8,58 @@
 '''
 
 
+n = int(input('Введите число: '))
+del_list_all = []
+del_list_simple = []
+
+
 def divisor_master():
-    n = int(input('Введите число: '))
-    del_list = []
-    del_list_2 = []
     if n > 0 and n < 1001:
         print('Число лежит в диапазоне от 1 до 1000')
         for i in range(1, 1001):
             if n % i == 0:
-                del_list.append(i)
+                del_list_all.append(i)
             else:
                 pass
-        print('Список всех делителей: ', del_list)
-        for i in del_list:
+        print('Список всех делителей: ', del_list_all)
+        for i in del_list_all:
             for j in range(2, i):
                 if i % j == 0:
                     break
             else:
-                del_list_2.append(i)
-        print('Наибольший простой делитель числа: ', sorted(del_list_2, reverse=True)[0])
+                del_list_simple.append(i)
+        print('Наибольший простой делитель числа: ', sorted(del_list_simple, reverse=True)[0])
+        del_list_simple.remove(1)
     else:
         print('Число меньше 1 или больше 1000.')
 
 divisor_master()
+
+
+
+'''функция выводит каноническое разложение числа на простые множители'''
+
+def diveser(n):
+    list = []
+    for i in del_list_simple:
+        while i*i <= n:
+            if n % i ==0:
+                list.append(i)
+                n //= i
+            else:
+                i += 1
+        if n > 1:
+            list.append(n)
+        return list
+
+print('Каноническое разложение числа на простые множители: ', diveser(n))
+
+
+
+'''функция выводит самый большой делитель (не обязательно простой) числа.'''
+
+print('Самый большой делитель (не обязательно простой) числа: ', sorted(del_list_all, reverse=True)[0])
+
+
+
 
