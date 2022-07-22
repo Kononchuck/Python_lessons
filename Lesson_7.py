96427bfed0dde2b66a5f5bbfd10ed1c6599548bc
office <d.kononchuk@outlook.com>
2022-07-22 10:56:55 +0300

    sa

--- Lesson_7.py
+++ Lesson_7.py
@@ -2,11 +2,20 @@ import pandas as pd
 import random
 import csv
 
-#df = pd.read_csv('Auto.csv')
-with open ('Auto.csv', 'r') as f:
-    reader = csv.reader(f, delimiter = '&')
-    next(reader) #пропускаем заголовок
-    df_auto = random.choice(list(reader))
+# with open ('Auto.csv', 'r') as f:
+#     reader = csv.reader(f, delimiter = '&')
+#     next(reader) #пропускаем заголовок
+#     df_auto = random.choice(list(reader))#выбираем случаную строку
+#     df_auto = ''.join(df_auto)#убираем скобы
+
+dict_new = []
+with open('Auto.csv', 'r') as data:
+    for line in csv.DictReader(data):
+        dict_new.append(line)
+
+print(random.choice(dict_new))
+
+
 
 
 
@@ -31,20 +40,18 @@ doc.save("generated_docx.docx")
 """
 
 # возвращает словарь аргуменов
-def get_context(mpg, cylinders, displacement, horsepower, weight, acceleration, year, origin, name):
-    return {
-        'mpg': mpg,
-        'cylinders': cylinders,
-        'displacement': displacement,
-        'horsepower': horsepower,
-        'weight': weight,
-        'acceleration': acceleration,
-        'year': year,
-        'origin': origin,
-        'name': name,
+# def get_context(mpg, cylinders, displacement, horsepower, weight, acceleration, year, origin, name):
+#     return {
+#         'mpg': mpg,
+#         'cylinders': cylinders,
+#         'displacement': displacement,
+#         'horsepower': horsepower,
+#         'weight': weight,
+#         'acceleration': acceleration,
+#         'year': year,
+#         'origin': origin,
+#         'name': name,
+#
+#     }
 
-    }
-
-print(df_auto)
-print(get_context(df_auto))
 
