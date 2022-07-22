dcb754388f15299c8908a43846cfba02af836f5a
office <d.kononchuk@outlook.com>
2022-07-22 08:39:00 +0300

    sa

--- Lesson_7.py
+++ Lesson_7.py
@@ -1,45 +1,47 @@
 import pandas as pd
+import random
+import csv
 
-df = pd.read_csv('Auto.csv')
-print(df)
+#df = pd.read_csv('Auto.csv')
+with open ('Auto.csv', 'r') as f:
+    reader = csv.reader(f, delimiter = '&')
+    print(random.choice(list(reader)))
+
 
 
 import datetime
 
-
-#from docxtpl import DocxTemplate
-#from docxtpl import InlineImage
 from docx.shared import Cm
 from docxtpl import DocxTemplate, InlineImage
 
+"""
+# определяем словарь переменных контекста,
+# которые определены в шаблоне документа DOCX
+context = {}
+context['company_name'] = 'Mechantica, Inc.'
 
-def get_context(company, result_sku_list): # возвращает словарь аргуменов
+doc = DocxTemplate("word_tpl.docx")
+#img = InlineImage(doc, image_descriptor='python_logo.png', width=Mm(20), height=Mm(10))
+
+# подставляем контекст в шаблон
+doc.render(context)
+# сохраняем и смотрим, что получилось
+doc.save("generated_docx.docx")
+"""
+
+# возвращает словарь аргуменов
+def get_context(mpg, cylinders, displacement, horsepower, weight, acceleration, year, origin, name):
     return {
-        'retailer': company,
-        'sku_list': result_sku_list,
+        'mpg': mpg,
+        'cylinders': cylinders,
+        'displacement': displacement,
+        'horsepower': horsepower,
+        'weight': weight,
+        'acceleration': acceleration,
+        'year': year,
+        'origin': origin,
+        'name': name,
+
     }
+print(get_context(18,8,307,130,3504,12,70,1,"chevrolet chevelle malibu"))
 
-
-def from_template(company, result_sku_list, template, signature):
-    template = DocxTemplate(template)
-    context = get_context(company, result_sku_list)  # gets the context used to render the document
-
-    img_size = Cm(15)  # sets the size of the image
-    acc = InlineImage(template, signature, img_size)
-
-    context['acc'] = acc  # adds the InlineImage object to the context
-    template.render(context)
-
-    template.save(company + '_' + str(datetime.datetime.now().date()) + '_report.docx')
-
-
-def generate_report(company, result_sku_list):
-    template = 'report.docx'
-    signature = 'acc.png'
-    document = from_template(company, result_sku_list, template, signature)
-
-
-def toFixed(numObj, digits=0):
-    return f"{numObj:.{digits}f}"
-
-generate_report('Ozon', [0.78, 0.12, 0.05, 0.01, 0.01])
