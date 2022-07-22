dd6918748928801833a10820499ea071e03c973f
office <d.kononchuk@outlook.com>
2022-07-22 18:12:22 +0300

    sa

--- Lesson_7.py
+++ Lesson_7.py
@@ -1,30 +1,24 @@
 import pandas as pd
 import random
 import csv
-
-# with open ('Auto.csv', 'r') as f:
-#     reader = csv.reader(f, delimiter = '&')
-#     next(reader) #пропускаем заголовок
-#     df_auto = random.choice(list(reader))#выбираем случаную строку
-#     df_auto = ''.join(df_auto)#убираем скобы
-
-dict_new = []
-with open('Auto.csv', 'r') as data:
-    for line in csv.DictReader(data):
-        dict_new.append(line)
-
-print(random.choice(dict_new))
-
-
-
-
-
 import datetime
-
 from docx.shared import Cm
 from docxtpl import DocxTemplate, InlineImage
 
-"""
+
+
+def get_context():
+    dict_new = []
+    with open('Auto.csv', 'r') as data:
+        for line in csv.DictReader(data):
+            dict_new.append(line)
+        return random.choice(list(dict_new))
+
+print(get_context())
+
+
+
+
 # определяем словарь переменных контекста,
 # которые определены в шаблоне документа DOCX
 context = {}
@@ -37,7 +31,7 @@ doc = DocxTemplate("word_tpl.docx")
 doc.render(context)
 # сохраняем и смотрим, что получилось
 doc.save("generated_docx.docx")
-"""
+
 
 # возвращает словарь аргуменов
 # def get_context(mpg, cylinders, displacement, horsepower, weight, acceleration, year, origin, name):
@@ -51,7 +45,6 @@ doc.save("generated_docx.docx")
 #         'year': year,
 #         'origin': origin,
 #         'name': name,
-#
 #     }
 
 
