e56dd755d53cc89394c1a1beb5e04a77aaa034d8
den <7735440@gmail.com>
2022-07-23 08:29:57 +0300

    code added

--- Lesson_7.py
+++ Lesson_7.py
@@ -1,50 +1,34 @@
-import pandas as pd
 import random
 import csv
-import datetime
-from docx.shared import Cm
-from docxtpl import DocxTemplate, InlineImage
+import json
+import time
+from docxtpl import DocxTemplate
 
-
-
+#начинаем замерять время на выполнение задач
+start = time.process_time()
+#Создайте csv-файл с данными о машине.
 def get_context():
     dict_new = []
     with open('Auto.csv', 'r') as data:
-        for line in csv.DictReader(data):
-            dict_new.append(line)
+        for i in csv.DictReader(data):
+            dict_new.append(i)
         return random.choice(list(dict_new))
 
-print(get_context())
+#Создайте шаблон документа doc
+doc = DocxTemplate('auto_tpl.docx')
 
+content = get_context()
 
+#подсчитываем время на выполнение задач
+content['time'] = time.process_time() - start
 
+# подставляем затраченное время в шаблон
+doc.render(content)
 
-# определяем словарь переменных контекста,
-# которые определены в шаблоне документа DOCX
-context = {}
-context['company_name'] = 'Mechantica, Inc.'
-
-doc = DocxTemplate("word_tpl.docx")
-#img = InlineImage(doc, image_descriptor='python_logo.png', width=Mm(20), height=Mm(10))
-
-# подставляем контекст в шаблон
-doc.render(context)
-# сохраняем и смотрим, что получилось
+# Вносим данные из файла в шаблон
 doc.save("generated_docx.docx")
 
-
-# возвращает словарь аргуменов
-# def get_context(mpg, cylinders, displacement, horsepower, weight, acceleration, year, origin, name):
-#     return {
-#         'mpg': mpg,
-#         'cylinders': cylinders,
-#         'displacement': displacement,
-#         'horsepower': horsepower,
-#         'weight': weight,
-#         'acceleration': acceleration,
-#         'year': year,
-#         'origin': origin,
-#         'name': name,
-#     }
-
+#Создайте json-файл с данными о машине.
+dict_to_json = json.dumps(get_context())
+print(type(dict_to_json), dict_to_json)
 
