a84aebb089455bd3d99a76fd493ed45ac13fcdb4
office <d.kononchuk@outlook.com>
2022-07-29 08:41:25 +0300

    sa

--- Lesson_8.py
+++ Lesson_8.py
@@ -1,5 +1,10 @@
+import os
+
 import names
 import time
+
+import netbios
+import psutil
 
 #1. Написать свой генератор последовательностей, свой тернарный оператор
 
@@ -12,18 +17,47 @@ check_num = ['Есть число!' for i in gen_simple if i == L]
 print(check_num)
 
 
-#2. Написать свой декоратор
+# 2. Написать свой декоратор
+# 2. Написать декоратор, замеряющий объем оперативной памяти, потребляемый декорируемой функцией.
+# 3. 4. Сравнить объем оперативной памяти для функции создания генератора и
+# функции создания списка с элементами: натуральные числа от 1 до 1000000.
 
-
-def simple_func():
-    print('Hi')
-
-def check_time():
+def check_time(f):
     start_time = time.process_time()#начинаем замерять время на выполнение задач
-    simple_func()
-    check_num
-    stop_time = time.process_time() - start_time#подсчитываем время на выполнение задач
+    #print(start_time)
+    f()
+    stop_time = time.process_time()-start_time #подсчитываем время на выполнение задач
+    #print(stop_time)
     return stop_time
 
+def check_memory(f):
+    start_proc = psutil.Process(os.getpid())#начинаем замерять время на выполнение задач
+    print('Использовано памяти до выполнения функции: ' + str(start_proc.memory_info().rss/1000000))
+    f()
+    stop_proc = psutil.Process(os.getpid()) #подсчитываем время на выполнение задач
+    #sum_proc = float(stop_proc.memory_info().rss/1000000) - float(start_proc.memory_info().rss/1000000)
+    print('Использовано памяти после выполнения функции: ' + str(stop_proc.memory_info().rss/1000000))
+    #print(sum_proc)
+    #return sum_proc
 
-print(check_time())
+
+@check_time
+def simple_func():
+    n = []
+    for i in range(1000000):
+        n.append(i)
+    return n
+print(simple_func)
+
+
+@check_memory
+def simple_func():
+    n = []
+    for i in range(10000000):
+        n.append(i)
+    return n
+print(simple_func)
+
+
+simple_func_gen = (i for i in range(10000000))
+print(simple_func_gen)
