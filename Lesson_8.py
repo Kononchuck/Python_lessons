7372986cbb90af5cc392e9022201a27e8a6cca7a
den <7735440@gmail.com>
2022-07-30 08:24:36 +0300

    code added

--- Lesson_8.py
+++ Lesson_8.py
@@ -1,20 +1,21 @@
 import os
+import sys
 
 import names
 import time
 
-import netbios
+
 import psutil
 
 #1. Написать свой генератор последовательностей, свой тернарный оператор
 
-N = 100
+N = 1000000
 gen_simple = [i**2 for i in range (N+2)]
-print(gen_simple)
+#print(gen_simple)
 
 L = 36
 check_num = ['Есть число!' for i in gen_simple if i == L]
-print(check_num)
+#print(check_num)
 
 
 # 2. Написать свой декоратор
@@ -24,10 +25,11 @@ print(check_num)
 
 def check_time(f):
     start_time = time.process_time()#начинаем замерять время на выполнение задач
-    #print(start_time)
+    #print('Начало отсчета: ', start_time)
     f()
     stop_time = time.process_time()-start_time #подсчитываем время на выполнение задач
-    #print(stop_time)
+    #print('Затрачено времени на выполнение функции: ', time.process_time())
+    #print('Времени занято процессом: ', stop_time)
     return stop_time
 
 def check_memory(f):
@@ -35,32 +37,33 @@ def check_memory(f):
     print('Использовано памяти до выполнения функции: ' + str(start_proc.memory_info().rss/1000000))
     f()
     stop_proc = psutil.Process(os.getpid()) #подсчитываем время на выполнение задач
-    #sum_proc = float(stop_proc.memory_info().rss/1000000) - float(start_proc.memory_info().rss/1000000)
     print('Использовано памяти после выполнения функции: ' + str(stop_proc.memory_info().rss/1000000))
     #print(sum_proc)
     #return sum_proc
 
 
 @check_time
-def simple_func():
+def simple_func_list():
     n = []
     for i in range(1000000):
         n.append(i)
     return n
-print(simple_func)
+print('Затрачено времени на выполнение функции: ', simple_func_list)
 
 
 @check_memory
-def simple_func():
+def simple_func_list():
     n = []
     for i in range(1000000):
         n.append(i)
     return n
-print(simple_func)
-
-
-simple_func_gen = (i for i in range(1000000))
+print(simple_func_list)
 
 
 
+gen_simple_one = [i for i in range (1000000)]
+gen_simple_two = (i for i in range (1000000))
+
+print('Memory: ', sys.getsizeof(gen_simple_one))
+print('Memory: ', sys.getsizeof(gen_simple_two))
 
