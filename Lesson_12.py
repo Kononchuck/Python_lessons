c2e84c882a92c6e1cea70f6317b1d6c4c6b0e3b8
office <d.kononchuk@outlook.com>
2022-08-26 07:08:48 +0300

    ok

--- Lesson_12.py
+++ Lesson_12.py
@@ -1,33 +1,10 @@
+import json
+
 import requests
 import pprint
 
-#https://github.com/hhru/api/blob/master/docs/vacancies.md#search
-#https://github.com/hhru/api
-#
-# domain = 'https://api.hh.ru/'
-#
-# url_vac = f'{domain}vacancies'
-#
-# params = {
-#     'text': 'python developer',
-#     # page
-#     'page': 100
-# }
-#
-# result = requests.get(url_vac, params=params)
-#
-# # items = result['items']
-# #
-# # first = items[0]
-#
-# #print(result.status_code)
-#
-# # data = result.json()
-# pprint.pprint(result.json())
-# # pprint.pprint(first)
-# # print(first['name'])
-# # print(first['schedule'])
-# # print(first['salary'])
+# #https://github.com/hhru/api/blob/master/docs/vacancies.md#search
+# #https://github.com/hhru/api
 """
 1. Используя HH API, рассчитать среднюю зарплату в Москве по запросу "Python"
 2. Составить список релевантных навыков по вакансиям Python-разработчик.
@@ -38,30 +15,65 @@ import pprint
 ● считаем частоту появления ключевых слов в текстах вакансий;
 ● выводим ТОП-10 навыков и процент их встречаемости пользователю."""
 
-list_of_vacancies = []
+
 all_salaries = 0
 all_num = 0
+all_words = 0
 
-for i in range(100):
-    url = 'https://api.hh.ru/vacancies'
-    result = requests.get(url, params={'text': 'python developer', 'area': '1', 'page' : i})
-    list_of_vacancies.append(result.json())
+
+# list_of_vacancies = []
+# for i in range(100):
+#     url = 'https://api.hh.ru/vacancies'
+#     result = requests.get(url, params={'text': 'Python', 'area': '1', 'per_page': 1, 'page' : i})
+#     list_of_vacancies.append(result.json())
+# with open('vacancies.txt', 'w') as outfile:
+#     json.dump(list_of_vacancies, outfile)
+
+"""Сохраняем данные для локальной работы"""
+with open('vacancies.txt') as json_file:
+    list_of_vacancies = json.load(json_file)
+
+
+
+# for i in list_of_vacancies:
+#     y = i['items']
+#     num = 0
+#     sum_salaries = 0
+#     for i in y:
+#         if i['salary'] !=None:
+#             salary = i['salary']
+#             if salary['from'] !=None:
+#                 num += 1
+#                 salary['from']
+#                 sum_salaries += salary['from']
+#     all_salaries += sum_salaries
+#     all_num += num
+# average_salary = all_salaries/all_num
 
 for i in list_of_vacancies:
     y = i['items']
     num = 0
     sum_salaries = 0
     for i in y:
-        if i['salary'] !=None:
-            salary = i['salary']
-            if salary['from'] !=None:
-                num += 1
-                salary['from']
-                sum_salaries += salary['from']
-    all_salaries += sum_salaries
-    all_num += num
-average_salary = all_salaries/all_num
+        if i['snippet'] == 'MongoDB':
+            num += 1
+    all_words += num
+print(all_words)
 
 
-print(average_salary)
-pprint.pprint(list_of_vacancies[0])
+
+
+# print(average_salary)
+#pprint.pprint(list_of_vacancies[20])
+
+
+#
+# for i in list_of_vacancies['items']:
+#     print('requirement': + 'Django')
+#
+#
+# for p in data['people']:
+#         print('Name: ' + p['name'])
+#         print('Website: ' + p['website'])
+#         print('From: ' + p['from'])
+#         print('')
