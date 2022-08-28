84b40c81ea7edd123838542b239101559049fe0a
den <7735440@gmail.com>
2022-08-28 08:29:32 +0300

    code added

--- QMS.py
+++ QMS.py
@@ -10,15 +10,18 @@ ses.headers = {'HH-User-Agent': "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/
 all_salaries = 0
 all_num = 0
 all_words = 0
+search_num = 0
 
 """Закомментированный код используется один раз для получения информации с HH"""
 list_of_vacancies_qms = []
 for i in range(100):
     url = 'https://api.hh.ru/vacancies'
-    result = requests.get(url, params={'text': 'специалист службы качества', 'per_page': 1, 'page' : i})
+    result = requests.get(url, params={'text': 'Quality manager', 'area': '1', 'per_page': 1, 'page' : i})
     list_of_vacancies_qms.append(result.json())
-print(type(list_of_vacancies_qms))
+    search_num += 1
 
+print(type(list_of_vacancies_qms))
+print("Всего найдено анкет: ", search_num)
 
 """Сохраняем данные для локальной работы"""
 with open('vacancies_qms.txt', 'w') as outfile:
@@ -47,18 +50,19 @@ average_salary = all_salaries/all_num
 average_salary = float('{:.2f}'.format(average_salary))
 print("Средняя заработная плата специалистов по качеству в Москве (рубли): ", average_salary)
 
-
+all_qms = 0
 tags_list_vac_qms = []
 for i in list_of_vacancies_qms:
     for i in i['items']:
         vac_id = i['id']
         vac_res = ses.get(f'https://api.hh.ru/vacancies/{vac_id}')
         if len(vac_res.json()["key_skills"]) > 0:  # at least one skill present
-            print(vac_id)
+            all_qms += 1
+            # print(vac_id)
             tags = [v for v_dict in vac_res.json()["key_skills"] for _, v in v_dict.items()]
-            print(' '.join(tags))
+            # print(' '.join(tags))
             tags_list_vac_qms.append(tags)
-            print()
+            # print()
             """Сохраняем данные для локальной работы"""
             with open('tags_list_vac_qms.txt', 'w') as outfile:
                 json.dump(tags_list_vac_qms, outfile)
@@ -77,3 +81,4 @@ for sublist in tags_list_vac_qms:
             counts[x] = 1
 
 print("Выводим ТОП-10 навыков и процент их встречаемости: ", sorted(counts.items(), reverse=True, key=lambda x: x[1])[0:10])
+print("Всего анкет: ", all_qms)
